0de3f355fdeb4ea28e3ad08df86cb12ecf665513
WalterZhu215 <zhiwei.zhu@jcu.edu.au>
2025-02-03 15:39:01 +0800

    print password stars

---
+++ prac_02/password_stars.py
@@ -0,0 +1,22 @@
+# PASSWORD_LENGTH = 10
+# def main():
+#     password = input("Enter your password: ")
+#
+#     while len(password) < PASSWORD_LENGTH:
+#         print(f"Password must be at least {PASSWORD_LENGTH} characters long.")
+#         password = input("Enter your password: ")
+#
+#     print('*' * len(password))
+#
+# main()
+
+PASSWORD_LENGTH = 10
+
+password = input("Enter password: ")
+while password <= PASSWORD_LENGTH:
+    print("Invalid Password")
+    password = input("Enter password: ")
+
+length = len(password)
+
+print(length * "*")
