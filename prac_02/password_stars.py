0ad70947ad2389bada7b4a2807189d4319d69fb1
WalterZhu215 <zhiwei.zhu@jcu.edu.au>
2025-02-07 11:37:48 +0800

    reflection

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,23 +1,33 @@
-# PASSWORD_LENGTH = 10
-# def main():
-#     password = input("Enter your password: ")
-#
-#     while len(password) < PASSWORD_LENGTH:
-#         print(f"Password must be at least {PASSWORD_LENGTH} characters long.")
-#         password = input("Enter your password: ")
-#
-#     print('*' * len(password))
-#
-# main()
+"""
+function main
+    get password
+
+    while len(password) < PASSWORD_LENGTH:
+        display "Password must be at least {10} characters long."
+
+    display '*' * len(password)
+"""
 
 PASSWORD_LENGTH = 10
+def main():
+    password = input("Enter your password: ")
 
-password = input("Enter password: ")
-while password <= PASSWORD_LENGTH:
-    print("Invalid Password")
-    password = input("Enter password: ")
+    while len(password) < PASSWORD_LENGTH:
+        print(f"Password must be at least {PASSWORD_LENGTH} characters long.")
+        password = input("Enter your password: ")
 
-length = len(password)
+    print('*' * len(password))
 
-print(length * "*")
+main()
+
+# PASSWORD_LENGTH = 10
+#
+# password = input("Enter password: ")
+# while password <= PASSWORD_LENGTH:
+#     print("Invalid Password")
+#     password = input("Enter password: ")
+#
+# length = len(password)
+#
+# print(length * "*")
 
