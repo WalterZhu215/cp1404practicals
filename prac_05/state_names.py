959c90bed0005cc7738122d4c1bb240487c339fa
WalterZhu215 <zhiwei.zhu@jcu.edu.au>
2025-02-24 15:48:32 +0800

    print state_names

---
+++ prac_05/state_names.py
@@ -0,0 +1,21 @@
+def main():
+    incomes = []
+    month_number = int(input("How many months? "))
+
+    for month in range(1, month_number + 1):
+        income = float(input(f"Enter income for month {month}:"))
+        incomes.append(income)
+
+    print("\nIncome Report\n-------------")
+    print_report(incomes, month_number)
+
+
+def print_report(incomes, month_number):
+    total = 0
+    for month in range(1, month_number + 1):
+        income = incomes[month - 1]
+        total += income
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+
+
+main()
